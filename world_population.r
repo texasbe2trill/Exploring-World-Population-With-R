d4bb8bfae20a786467bdd280335581613edd66f1
texasbe2trill <91504044+texasbe2trill@users.noreply.github.com>
2024-01-08 17:06:14 -0600

    Add scatterplot and top_population
    Filters by top countries and adds a scatterplot.

--- world_population.r
+++ world_population.r
@@ -1,12 +1,37 @@
 # Load the dplyr and ggplot2 libraries
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
 
 # Import the world_population.csv dataset
 world_population <- read.csv("world_population_data.csv")
 
+# Create data for world coordinates using map_data
+world_coordinates <- map_data("world")
+
 # Filter the world_population dataset
+top_population <- world_population %>%
+  filter(
+    country %in% c(
+      "India", "China", "United States",
+      "Indonesia", "Pakistan", "Nigeria",
+      "Brazil", "Bangladesh", "Russia",
+      "Mexico", "Ethiopia", "Japan",
+      "Philippines", "Egypt", "DR Congo",
+      "Vietnam", "Iran", "Turkey",
+      "Germany", "Thailand", "United Kingdom"
+    )
+  ) %>%
+  mutate("population_density" = population_2023 / area_kms_squared)
 
 # Create a scatterplot using ggplot2
-print(ggplot(world_population, aes(x = continent, y = growth.rate)) +
-  geom_point())
+print(ggplot() +
+  # geom_map(
+  #   data = world_coordinates, map = world_coordinates,
+  #   aes(long, lat, map_id = region), color = "green", fill = "lightyellow"
+  # ) +
+  geom_point(data = top_population, aes(
+    x = growth_rate, y = population_density,
+    color = population_density,
+    size = population_2023
+  )))
