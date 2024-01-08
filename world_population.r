2e479793b6387dba924b27d9a92ced173d414088
texasbe2trill <91504044+texasbe2trill@users.noreply.github.com>
2024-01-08 10:21:10 -0600

    Add files via upload
    Add initial world_population_data.csv dataset and world_population.r files.

---
+++ world_population.r
@@ -0,0 +1,9 @@
+# Load the dplyr and ggplot2 libraries
+library(dplyr)
+library(ggplot2)
+
+# Import the world_population.csv dataset
+world_population <- read.csv("world_population_data.csv")
+
+# Print world_population dataset
+print(world_population)
