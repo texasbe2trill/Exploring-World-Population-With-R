0ba2793a3a6da1e26b12a6b13e445ed803c751bd
texasbe2trill <91504044+texasbe2trill@users.noreply.github.com>
2024-01-09 15:39:15 -0600

    Update world_population.r
    World map shows with growth_rate as fill value. Added the ability to save the plot.

--- world_population.r
+++ world_population.r
@@ -8,22 +8,28 @@ world_population <- read.csv("world_population_data.csv")
 
 # Remove '%' from growth_rate column
 world_population <- world_population %>%
-  mutate(
-    growth_rate = as.numeric(sub("%", "", growth_rate)),
-    growth_rate_multiple = growth_rate * 100
-  )
+        mutate(growth_rate = as.numeric(sub("%", "", growth_rate)))
 
-# Create data for world coordinates using map_data
+# Create dataframe for world coordinates using map_data
 world_coordinates <- map_data("world")
 
-# Add simulated data and remove Antarctica
+# Add map_data data, remove Antarctica, and join by region
 world_map <- map_data("world") %>%
-  filter(region != "Antarctica") %>%
-  left_join(world_population, by = c("region" = "country"))
+        filter(region != "Antarctica") %>%
+        left_join(world_population, by = c("region" = "country"))
 
-# Create a scatterplot using ggplot2
+# Create a geom_polygon plot using ggplot2
 print(ggplot(world_map) +
-  geom_polygon(aes(long, lat, group = group, fill = growth_rate)) +
-  coord_quickmap() +
-  scale_fill_viridis_c(option = "plasma") +
-  theme_void())
+        geom_polygon(aes(long, lat, group = group, fill = growth_rate)) +
+        coord_quickmap() +
+        scale_fill_viridis_c(option = "plasma") +
+
+        # Add labels to the plot
+        labs(fill = "Growth %") +
+        ggtitle("Growth Rate by Region") +
+
+        # Tweak display of theme
+        theme_void())
+
+# Save the plot as a PNG file
+ggsave("plot.png", dpi = 300)
