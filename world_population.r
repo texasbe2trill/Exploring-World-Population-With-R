178aef60e4da33fd8c4adf855ffd6e077165bf40
texasbe2trill <91504044+texasbe2trill@users.noreply.github.com>
2024-01-09 10:22:13 -0600

    Update world_population.r
    added growth_rate_multiple by removing % character and multiplying growth_rate * 100. World map displays without errors.

--- world_population.r
+++ world_population.r
@@ -4,14 +4,19 @@ library(ggplot2)
 library(tidyverse)
 
 # Import the world_population.csv dataset
-world_population <- read.csv("world_population_data.csv") %>%
-  mutate("population_density" = population_2023 / area_kms_squared)
+world_population <- read.csv("world_population_data.csv")
 
+# Remove '%' from growth_rate column
+world_population <- world_population %>%
+  mutate(
+    growth_rate = as.numeric(sub("%", "", growth_rate)),
+    growth_rate_multiple = growth_rate * 100
+  )
 
 # Create data for world coordinates using map_data
 world_coordinates <- map_data("world") %>%
   distinct(region) %>%
-  mutate(population_density = runif(n())) %>%
+  mutate(growth_rate_multiple = runif(n())) %>%
   slice_sample(n = 200)
 
 # Add simulated data and remove Antarctica
@@ -19,24 +24,10 @@ world_map <- map_data("world") %>%
   filter(region != "Antarctica") %>%
   cross_join(world_population)
 
-# Filter the world_population dataset
-# top_population <- world_population %>%
-#   filter(
-#     country %in% c(
-#       "India", "China", "United States",
-#       "Indonesia", "Pakistan", "Nigeria",
-#       "Brazil", "Bangladesh", "Russia",
-#       "Mexico", "Ethiopia", "Japan",
-#       "Philippines", "Egypt", "DR Congo",
-#       "Vietnam", "Iran", "Turkey",
-#       "Germany", "Thailand", "United Kingdom"
-#     )
-#   ) %>%
-#   mutate("population_density" = population_2023 / area_kms_squared)
 
 # Create a scatterplot using ggplot2
 print(ggplot(world_map) +
-  geom_polygon(aes(long, lat, group = group, fill = population_density)) +
+  geom_polygon(aes(long, lat, group = group, fill = growth_rate_multiple)) +
   coord_quickmap() +
   scale_fill_viridis_c(option = "plasma", na.value = NA) +
   theme_void())
