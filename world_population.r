ab94aed895c42e798242dd64ecf1b85ec62d3ea0
texasbe2trill <91504044+texasbe2trill@users.noreply.github.com>
2024-01-09 09:53:29 -0600

    Update world_population.r
    World map displays successfully with population_density

--- world_population.r
+++ world_population.r
@@ -4,34 +4,39 @@ library(ggplot2)
 library(tidyverse)
 
 # Import the world_population.csv dataset
-world_population <- read.csv("world_population_data.csv")
+world_population <- read.csv("world_population_data.csv") %>%
+  mutate("population_density" = population_2023 / area_kms_squared)
+
 
 # Create data for world coordinates using map_data
 world_coordinates <- map_data("world") %>%
-  filter(region != "Antarctica")
+  distinct(region) %>%
+  mutate(population_density = runif(n())) %>%
+  slice_sample(n = 200)
+
+# Add simulated data and remove Antarctica
+world_map <- map_data("world") %>%
+  filter(region != "Antarctica") %>%
+  cross_join(world_population)
 
 # Filter the world_population dataset
-top_population <- world_population %>%
-  filter(
-    country %in% c(
-      "India", "China", "United States",
-      "Indonesia", "Pakistan", "Nigeria",
-      "Brazil", "Bangladesh", "Russia",
-      "Mexico", "Ethiopia", "Japan",
-      "Philippines", "Egypt", "DR Congo",
-      "Vietnam", "Iran", "Turkey",
-      "Germany", "Thailand", "United Kingdom"
-    )
-  ) %>%
-  mutate("population_density" = population_2023 / area_kms_squared)
+# top_population <- world_population %>%
+#   filter(
+#     country %in% c(
+#       "India", "China", "United States",
+#       "Indonesia", "Pakistan", "Nigeria",
+#       "Brazil", "Bangladesh", "Russia",
+#       "Mexico", "Ethiopia", "Japan",
+#       "Philippines", "Egypt", "DR Congo",
+#       "Vietnam", "Iran", "Turkey",
+#       "Germany", "Thailand", "United Kingdom"
+#     )
+#   ) %>%
+#   mutate("population_density" = population_2023 / area_kms_squared)
 
 # Create a scatterplot using ggplot2
-print(ggplot() +
-  geom_map(
-    data = world_coordinates, map = world_coordinates,
-    aes(long, lat, map_id = region)
-  )) +
-  geom_point(data = top_population, aes(
-    color = population_density,
-    size = population_2023
-  ))
+print(ggplot(world_map) +
+  geom_polygon(aes(long, lat, group = group, fill = population_density)) +
+  coord_quickmap() +
+  scale_fill_viridis_c(option = "plasma", na.value = NA) +
+  theme_void())
